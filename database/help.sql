b448bc09e8c810320ff3182ccdbfccc5aefd3173
Ade Pangestu <adepanges@gmail.com>
2018-03-13 16:51:05 +0700

    note

--- database/help.sql
+++ database/help.sql
@@ -10,11 +10,7 @@ WHERE a.status = 1;
 -- manager
 INSERT INTO `sso_role_access` (`role_id`,`module_id`,`menu_id`,`feature_id`,`feature_name`,`status`)
     SELECT
-    2, a.module_id, b.menu_id, c.feature_id, c.name,
-    CASE WHEN
-    c.name IN (
-        'sso_users_del'
-    ) THEN 0 ELSE 1 END AS flag
+    2, a.module_id, b.menu_id, c.feature_id, c.name, 1
 FROM modules a
 LEFT JOIN module_menu b ON a.module_id = b.module_id AND b.status = 1
 LEFT JOIN module_feature c ON b.menu_id = c.menu_id AND b.status = 1
