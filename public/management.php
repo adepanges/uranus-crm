0064d39aacffefbbce2b4e16555201503787e5e5
Ade Pangestu <adepanges@gmail.com>
2018-04-03 13:44:41 +0700

    prod setting

--- public/management.php
+++ public/management.php
@@ -1,6 +1,6 @@
 <?php
 
-define('ENVIRONMENT', 'development');
+define('ENVIRONMENT', 'production');
 define('INDEX_PAGE', pathinfo(__FILE__, PATHINFO_BASENAME));
 
 $system_path = '../codeigniter/system_3.1.7';
