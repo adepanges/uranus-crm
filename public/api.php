36de1ee3d5eafb4817f421607ed878dcf2a70c4d
Ade Pangestu <adepanges@gmail.com>
2018-04-03 11:44:56 +0700

    dev envi

--- public/api.php
+++ public/api.php
@@ -1,6 +1,6 @@
 <?php
 
-define('ENVIRONMENT', 'production');
+define('ENVIRONMENT', 'development');
 define('INDEX_PAGE', pathinfo(__FILE__, PATHINFO_BASENAME));
 
 $system_path = '../codeigniter/system_3.1.7';
