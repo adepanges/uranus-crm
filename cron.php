973cca6670b0d9521f9f791b8da2aad581929d74
= <=>
2018-06-05 02:51:00 +0700

    fix envi prod

--- cron.php
+++ cron.php
@@ -1,9 +1,9 @@
 <?php
 
-define('ENVIRONMENT', 'development');
+define('ENVIRONMENT', 'production');
 define('INDEX_PAGE', pathinfo(__FILE__, PATHINFO_BASENAME));
 
-$system_path = 'codeigniter/system_3.1.7';
+$system_path = 'codeigniter/system_3.1.8';
 $application_folder = 'module/cron';
 
 require_once('launch.php');
